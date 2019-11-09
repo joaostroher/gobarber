609668eaab26f7b499db5aab8822330f136eac14
Joao Miguel Stroher <joao.stroher@gmail.com>
2019-11-09 18:15:29 -0300

    Profile and dashboard features

--- frontend-web/src/pages/Dashboard/index.js
+++ frontend-web/src/pages/Dashboard/index.js
@@ -1,9 +1,98 @@
-import React from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
+import {
+  format,
+  subDays,
+  addDays,
+  setMinutes,
+  setHours,
+  setSeconds,
+  isBefore,
+  isEqual,
+  parseISO,
+  setMilliseconds,
+} from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
+import pt from 'date-fns/locale/pt';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
+
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 
 export default function Dashboard() {
-  api.get('appointments?page=1');
-  return <h1>Dashboard</h1>;
+  const [date, setDate] = useState(
+    setMinutes(setSeconds(setMilliseconds(new Date(), 0), 0), 0)
+  );
+  const [schedules, setSchedules] = useState([]);
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  useEffect(() => {
+    async function loadSchedules() {
+      const response = await api.get('schedules', { params: { date } });
+
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      const data = range.map(hour => {
+        const checkDate = setHours(date, hour);
+        const compareDate = utcToZonedTime(checkDate, timezone);
+
+        const appointment = response.data.find(a =>
+          isEqual(parseISO(a.date), compareDate)
+        );
+
+        return {
+          time: `${hour}:00h`,
+          past: isBefore(compareDate, new Date()),
+          appointment,
+        };
+      });
+
+      setSchedules(data);
+    }
+    loadSchedules();
+  }, [date]);
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
+  return (
+    <Container>
+      <header>
+        <button type="button" onClick={handlePrevDay}>
+          <MdChevronLeft size={36} color="#FFF" />
+        </button>
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
+          <MdChevronRight size={36} color="#FFF" />
+        </button>
+      </header>
+      <ul>
+        {schedules.map(schedule => (
+          <Time
+            key={schedule.time}
+            past={schedule.past}
+            available={!schedule.appointment}
+          >
+            <strong>{schedule.time}</strong>
+            <span>
+              {schedule.appointment
+                ? schedule.appointment.user.name
+                : 'Em Aberto'}
+            </span>
+          </Time>
+        ))}
+      </ul>
+    </Container>
+  );
 }
